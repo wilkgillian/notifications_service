e2bb5090d00f377bd37ed42dd2cd09048a5ed2ba
wilkgillian <wilkrosa1998@gmail.com>
2022-12-16 03:31:27 -0400

    ..

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,41 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date) {
+    this.props.readAt = readAt;
+  }
+  public get readAt(): Date | null {
+    return this.props.readAt;
+  }
+}
